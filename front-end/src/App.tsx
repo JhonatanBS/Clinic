cc25d986f7beb7836f582f34c001ee1b4c4050fe
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2024-04-11 17:28:33 -0300

    Creating section main from application

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,19 @@
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
 import { Header } from './components/Header';
+import { Main } from './components/Main';
 
 function App() {
   return (
     <div className={styles.app}>
+
       <Header />
+      <Main />
+
+
+      <section>
+
+      </section>
     </div>
   );
 }
---
+++ front-end/src/components/Main/index.tsx
@@ -0,0 +1,30 @@
+import styles from "./styles.module.css";
+
+export function Main() {
+  return (
+    <main className={styles.home}>
+
+      <div className={styles.titleClinic}>
+        <div>
+          <h1>
+            Clinic
+          </h1>
+
+        </div>
+
+        <p>
+          Bem-vindo ao nosso espaço dedicado ao seu bem-estar.
+          Estamos aqui para ajudá-lo em cada passo do caminho.
+          onde sua saúde é nossa preocupação principal, e nossa
+          equipe está dedicada a fornecer o melhor cuidado
+          possível em cada consulta.
+        </p>
+      </div>
+
+      <div className={styles.carouselPhotos}>
+
+      </div>
+
+    </main>
+  )
+}
