348219943881b122d2e95cc362d55fb0d4919d7e
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2024-04-15 15:21:10 -0300

    Finishing the footer

--- front-end/src/components/Footer/index.tsx
+++ front-end/src/components/Footer/index.tsx
@@ -5,7 +5,13 @@ import styles from "./styles.module.css";
 export function Footer() {
   return (
     <footer className={styles.containerFooter}>
-      
+      <p>Copyright &copy;2024 Clínica Médica <strong>clinic</strong>. Todos os direitos reservados.</p>
+
+      <p>Horario de Funcionamento</p>
+
+      <p className={styles.timeFooter}>Segunda à Sexta  (07:00 - 18:00)</p>
+      <p className={styles.timeFooter}>Sábado  (07:00 - 12:00)</p>
+      <p className={styles.timeFooter}>Domingos e Feriados são fechados</p>
     </footer>
   )
 }
